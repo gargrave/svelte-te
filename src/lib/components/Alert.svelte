ad2dfe54fb9be870c0bc937c1dc19586ccdd85d0
Gabe Hargrave <gabe.hargrave@vacasa.com>
2023-06-11 13:35:27 -0700

    Just some cleanup

--- src/lib/components/Alert.svelte
+++ src/lib/components/Alert.svelte
@@ -4,27 +4,8 @@
 	import Icon from './Icon.svelte';
 
 	import type { ColorType } from './components.types';
+	import { iconsByType, textAndBgByColorType } from './components.utils';
 	import type { IconType } from './svg/IconMap';
-
-	// TODO: pull from utils
-	const iconsByType: Record<ColorType, IconType> = {
-		primary: 'InfoCircle',
-		secondary: 'InfoCircle',
-		success: 'CheckCircle',
-		danger: 'XCircle',
-		warning: 'ExclamationTriangle',
-		info: 'InfoCircle'
-	};
-
-	// TODO: pull from utils
-	const colorTypeStyles: Record<ColorType, string> = {
-		primary: 'bg-primary-100 text-primary-600',
-		secondary: 'bg-secondary-200 text-secondary-800',
-		success: 'bg-success-100 text-success-700',
-		danger: 'bg-danger-100 text-danger-700',
-		warning: 'bg-warning-100 text-warning-800',
-		info: 'bg-info-100 text-info-800'
-	};
 
 	export let colorType: ColorType = 'primary';
 	export let hideIcon = false;
@@ -34,7 +15,7 @@
 
 <div
 	class="mb-4 px-5 py-4 flex items-center justify-start gap-2
-		rounded-lg {colorTypeStyles[colorType]}"
+		rounded-lg {textAndBgByColorType[colorType]}"
 	role="alert">
 	{#if !hideIcon}
 		<Icon classes="shrink-0" icon={customIcon || iconsByType[colorType]} size={26} />
