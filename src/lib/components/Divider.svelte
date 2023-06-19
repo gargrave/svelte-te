cc26cec0ae6cc979311e4894a0febd6816d575bc
Gabe Hargrave <gabe.hargrave@vacasa.com>
2023-06-19 12:07:17 -0700

    Divider styling uptes

--- src/lib/components/Divider.svelte
+++ src/lib/components/Divider.svelte
@@ -1,2 +1,6 @@
 <!-- https://tailwind-elements.com/docs/standard/content-styles/dividers -->
-<hr class="my-6 h-0.5 border-t-0 bg-zinc-500 dark:bg-zinc-500" />
+<script>
+	export let classes = '';
+</script>
+
+<hr class="my-6 h-[1px] border-t-0 bg-zinc-500 dark:bg-zinc-600 {classes}" />
