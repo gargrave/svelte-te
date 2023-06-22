6bb5a68be14de38880e2db36e9286d5c7a02db0d
Gabe Hargrave <gabe.hargrave@vacasa.com>
2023-06-22 12:22:58 -0700

    reformat

--- src/lib/components/modal/Modal.svelte
+++ src/lib/components/modal/Modal.svelte
@@ -64,22 +64,30 @@
 	aria-hidden="true"
 	data-te-modal-init
 	class="fixed left-0 top-0 z-[1055] hidden h-full w-full
-		overflow-y-auto overflow-x-hidden outline-none">
+		overflow-y-auto overflow-x-hidden outline-none"
+>
 	<div
 		data-te-modal-dialog-ref
 		class="_modal pointer-events-none relative w-auto mx-auto opacity-0
 			transition-all duration-300 ease-in-out translate-y-[-50px]"
-		style:max-width="{maxWidth}px">
+		style:max-width="{maxWidth}px"
+	>
 		<div
 			class="pointer-events-auto relative flex w-full flex-col rounded-md
 				border-none bg-white bg-clip-padding text-current shadow-lg outline-none
 				top-1/2 translate-y-[-50%]
-				dark:bg-zinc-800 min-[576px]:shadow-[0_0.5rem_1rem_rgba(#000, 0.15)]">
+				dark:bg-zinc-800 min-[576px]:shadow-[0_0.5rem_1rem_rgba(#000, 0.15)]"
+		>
 			<!-- ModalHeader -->
 			<div
-				class="p-4 flex flex-shrink-0 items-center justify-between rounded-t-md border-b border-zinc-600">
+				class="p-4 flex flex-shrink-0 items-center justify-between rounded-t-md border-b border-zinc-600"
+			>
 				<!--Modal title-->
-				<Heading tag="h5" classes="text-xl font-medium leading-normal !mb-0" id={label}>
+				<Heading
+					tag="h5"
+					classes="text-xl font-medium leading-normal !mb-0"
+					id={label}
+				>
 					{title}
 				</Heading>
 				<slot name="header" />
